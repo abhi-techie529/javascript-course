bf5645d0bb8af2755d7853554da1576d7a07af38
abhi-techie <abhi-techie529@users.noreply.github.com>
2025-07-01 18:29:15 +0530

    problem 24

---
+++ 11_Array_and_loops/challange/11x.js
@@ -0,0 +1,74 @@
+ // When loading the page, load from localStorage.
+
+ const todoList = JSON.parse(localStorage.getItem('todoList')) || [{
+        name:"make dinner",
+        dueDate: "2025-12-22"
+        } ,{
+        name: "wash dishes" ,
+        dueDate: "2025-12-22"
+        } ];
+
+        renderTodoList();
+
+
+        function renderTodoList(){
+
+                    let todoListHTML = '';
+
+                for(let i = 0; i < todoList.length ; i++){
+                const todoObject = todoList[i];
+                // const name = todoObject.name;
+                // const dueDate = todoObject.dueDate;
+                //*********shorcut of above properties******//
+                const {name,dueDate} = todoObject;
+                const html = `
+                    <div> ${name}</div>
+                    <div> ${dueDate}</div>
+
+                    <button onclick = "
+                    todoList.splice(${i},1);
+                    renderTodoList();
+                    " class = "delete-todo-button">Delete</button>
+                `;
+                todoListHTML += html;
+                }
+        
+
+                document.querySelector('.js-todo-list').innerHTML = todoListHTML 
+
+        }
+
+
+
+         function addTodo(){
+            const inputElement = document.querySelector('.js-name-input');
+
+            const name = inputElement.value;     // to get the text out we use ( .value )
+
+            
+            const dateInputElement = document.querySelector('.js-duedate-input');
+            const dueDate = dateInputElement.value;               
+
+
+            
+            todoList.push({
+                // name:name ,
+                // dueDate: dueDate,
+                   //*********shorcut of above properties,if property:variable have same name ******//
+                 name ,
+                 dueDate });
+         
+            
+            inputElement.value = '';     // this is used to reset the values of the text input after adding the value
+
+            renderTodoList();
+
+             // Whenever we update the todo list, save in localStorage.
+            saveToStorage();
+
+         }
+
+         function saveToStorage(){
+            localStorage.setItem('todoList',JSON.stringify(todoList))
+         }
+
