541fb7b56fabf738418c224d2802a9f793fa620c
abhi-techie <abhi-techie529@users.noreply.github.com>
2026-06-03 19:57:12 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,19 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    [{
       productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity:2,
-},{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1,
-}];
+    },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:1,
+    }];
+}
 
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
 
@@ -32,8 +40,10 @@ export function addToCart(productId){
                         cart.push({
                         productId,
                         quantity
-                });
+                       });
                     }
+
+                    saveToStorage();
 
     }
 
@@ -47,4 +57,6 @@ export function addToCart(productId){
         });
 
         cart = newCart;
+
+        saveToStorage();
     }
