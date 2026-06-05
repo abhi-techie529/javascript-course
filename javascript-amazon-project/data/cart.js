6f1fa716482b5e8640f3c66b8d41939764dc3a8e
abhi-techie <abhi-techie529@users.noreply.github.com>
2026-06-05 17:41:38 +0530

    Finish the update feature

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,7 +1,7 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart){
-    [{
+    cart = [{
       productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity:2,
     },{
@@ -60,3 +60,28 @@ export function addToCart(productId){
 
         saveToStorage();
     }
+
+
+    export function calculateCartQuantity(){
+        let cartQuantity = 0;
+
+        cart.forEach((cartItem) => {
+            cartQuantity += cartItem.quantity;
+        });
+
+        return cartQuantity;
+    }
+
+    export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  saveToStorage();
+}
