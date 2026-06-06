6cc57ace8560328747eab97afcd931d8fb019f92
abhi-techie <abhi-techie529@users.noreply.github.com>
2026-06-06 17:34:03 +0530

    practice exercises

---
+++ 15_External_libraries/15f.js
@@ -0,0 +1,11 @@
+
+     
+
+
+        function isWeekend(date){
+            const dayOfWeek = date.format('dddd');
+            return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
+        }
+
+
+       export default isWeekend;
